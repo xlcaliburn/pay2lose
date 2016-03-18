ea775b337a367b4f3c02aa311e77cbd708df6fa8
xLCaliburn <michaelchunkitwong@gmail.com>
2016-03-18 14:38:53 -0400

    yo angular
    Generate angular files through grunt, yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pay2loseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pay2loseApp
+ */
+angular.module('pay2loseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pay2loseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pay2loseApp
+ */
+angular.module('pay2loseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
